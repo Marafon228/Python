07efa0aae1a0cf0e7b960add11799b2596011196
Marofon228 <denisoff.vaniok2016@mail.ru>
2021-10-06 20:29:57 +0300

    5

---
+++ начало.py
@@ -0,0 +1,26 @@
+# Начало( 2 видос)
+print("Not bed Not bed")
+# Начало ( 3 видос)
+print("WTF22")
+print("Stop", 22, "New", 3, ".", end="///\n")
+print("Try \" ", 67,  5, 'J"', sep="")
+print(" \' EXE", "\nI\nV\nV\nA\nN", 'R \t \\OOOK')
+# print(119 +54 -22)
+print(5*5)
+print(5 ** 3, 7/3, 7//3)
+# print("математические функции")
+print(min(55, 3, 0, -5, -12), max(123, -55, -1000, 500), "Модуль", abs(-300)
+      , "Степень", pow(10, 3), "Округление", round(55/33))
+# Получение данных
+# input("Введите свой возраст")
+# Урок № 4
+number = 5 # int
+digit = 4.56575 # float
+word = "результат:" # string
+boolean = True # bool
+str_num = '5' #string
+# print(word + str(digit))
+print(word + str(number + int(str_num)))
+del number
+number = 8
+print("результат:", number)
